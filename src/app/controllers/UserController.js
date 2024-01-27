abd0c95ee8870967e473f962e719fea7fa521ff6
weltonSantana <welton.santana@mail.uft.edu.br>
2024-01-27 11:16:07 -0300

    feat: add function update in user profile

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,19 +1,18 @@
 const { request, response } = require('express');
+const bcrypt = require('bcrypt');
 const UsersRepository = require('../repositories/UsersRepository');
 
-
 class UserController {
-
   async index(request, response) {
     const users = await UsersRepository.findAll();
     response.json(users);
   }
 
   async show(request, response) {
-		const { id } = request.params;
-		const user = await UsersRepository.findById(id);
+    const { id } = request.params;
+    const user = await UsersRepository.findById(id);
 
-		if(!user) {
+    if (!user) {
       return response.status(404).json({ error: 'User not found' });
     }
 
@@ -21,24 +20,24 @@ class UserController {
   }
 
   async store(request, response) {
-    const {name, email, password} = request.body;
+    const { name, email, password } = request.body;
 
     if (!name) {
-      return response.status(400).json({error: 'Name is required'});
+      return response.status(400).json({ error: 'Name is required' });
     }
 
-		if (!email) {
-      return response.status(400).json({error: 'Email is required'});
+    if (!email) {
+      return response.status(400).json({ error: 'Email is required' });
     }
 
-		if (!password) {
-      return response.status(400).json({error: 'Password is required'});
+    if (!password) {
+      return response.status(400).json({ error: 'Password is required' });
     }
 
     const UserExists = await UsersRepository.findByEmail(email);
 
-    if (UserExists){
-      return response.status(400).json({error: 'This e-mail is already in use'});
+    if (UserExists) {
+      return response.status(400).json({ error: 'This e-mail is already in use' });
     }
 
     const user = await UsersRepository.create(name, email, password);
@@ -46,8 +45,42 @@ class UserController {
     response.json(user);
   }
 
-  update() {
+  async update(request, response) {
+    const { id } = request.params;
+    const {
+      name, email, password, state, city,
+    } = request.body;
 
+    const UserExists = await UsersRepository.findById(id);
+    if (!UserExists) {
+      return response.status(404).json({ error: 'User not found' });
+    }
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    if (!email) {
+      return response.status(400).json({ error: 'Email is required' });
+    }
+
+    if (!password) {
+      return response.status(400).json({ error: 'Password is required' });
+    }
+
+    const UserByEmail = await UsersRepository.findByEmail(email);
+
+    if (UserByEmail && UserByEmail.id !== id) {
+      return response.status(400).json({ error: 'This e-mail is already in use' });
+    }
+
+    const passwordHash = await bcrypt.hash(password, 12);
+
+    const user = await UsersRepository.update(id, {
+      name, email, passwordHash, state, city,
+    });
+
+    response.json(user);
   }
 
   delete() {
