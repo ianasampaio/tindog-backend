758088d3b72b0d6bcfa5e9f991970a4e5bc2da93
ianasampaio <iana.sampaio06@gmail.com>
2024-03-07 10:41:55 -0300

    refactor: update create  and findByUserId functions in DogsRepository

--- src/app/repositories/DogsRepository.js
+++ src/app/repositories/DogsRepository.js
@@ -8,20 +8,21 @@ class DogsRepository {
     breed,
     age,
     description,
+		picture,
   ) {
     const [row] = await db.query(
       `
-      INSERT INTO dogs(user_id,name, gender, breed, age, description)
-      VALUES($1, $2, $3, $4, $5, $6)
+      INSERT INTO dogs(user_id,name, gender, breed, age, description, picture)
+      VALUES($1, $2, $3, $4, $5, $6, $7)
       RETURNING *
     `,
-      [idUser, name, gender, breed, age, description],
+      [idUser, name, gender, breed, age, description, picture],
     );
     return row;
   }
 
 	async findByUserId(userId) {
-		const [row] = await db.query('SELECT id FROM dogs WHERE user_id = $1', [userId]);
+		const [row] = await db.query('SELECT * FROM dogs WHERE user_id = $1', [userId]);
     return row;
 	}
 }
