dbe79c5f7e50c0787923e613a9952c9c4a5f0922
ianasampaio <iana.sampaio06@gmail.com>
2024-03-12 09:08:33 -0300

    feat: create dog's info route

--- src/routes/Dogs.js
+++ src/routes/Dogs.js
@@ -1,8 +1,8 @@
 const { Router } = require('express');
-
 const router = Router();
 const DogController = require('../app/controllers/DogController');
 
 router.post('/', DogController.store);
+router.get('/:id', DogController.show);
 
 module.exports = router;
